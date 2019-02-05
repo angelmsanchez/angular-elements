63d2d18838b0b3b8a92d70e34dd3af4cae848d7e
Angel Munoz <angelmsanchez85@gmail.com>
2019-02-05 12:34:58 +0100

    -Create custom element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
-import { createCustomElement } from '@angular/elements'
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
+import { MessageComponent } from './components/message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // AppComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  entryComponents: [
+    MessageComponent
+  ]
 })
 export class AppModule {
 
   constructor(
     private injector: Injector,
-  ) { }
+  ) {
+    const customElement = createCustomElement(MessageComponent, { injector });
+    customElements.define('app-message', customElement);
+  }
 
   ngDoBootstrap(): void {
-    const element = createCustomElement(AppComponent, { injector: this.injector });
-    customElements.define('my-angular-microapp', element);
+    // const element = createCustomElement(AppComponent, { injector: this.injector });
+    // customElements.define('my-angular-microapp', element);
   }
 }
---
+++ src/app/components/message/message.component.ts
@@ -0,0 +1,16 @@
+
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-message',
+  template: `
+    <h1 style='text-center'>{{title}}</h1>
+    <h2>hey {{name}} loving Angular Elements {{answer}}</h2>
+  `,
+  styles: ['h2 {color:red;}']
+})
+export class MessageComponent {
+  @Input() name: string;
+  @Input() answer: string;
+  title = 'Angular Elements';
+}
